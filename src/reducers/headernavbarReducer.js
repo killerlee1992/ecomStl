4ca5c5d5b452e388544cdad6fd5273395cf862d3
sjohnnlee <killerlee1992@gmail.com>
2020-11-06 17:39:23 -0800

    styled header and navbar components

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -15,7 +15,8 @@ const INITIAL_STATE = {
     ],
     navbarLinks: [
         {
-            title: 'account'
+            title: 'account',
+            active:'true'
         },
         {
             title: 'purchases'
