adc71cedf714209096a03427cb92d7e4aad3bc78
sjohnnlee <killerlee1992@gmail.com>
2020-11-25 21:31:09 -0800

    added password fields

--- src/components/account/accounInformationtForm.js
+++ src/components/account/accounInformationtForm.js
@@ -4,10 +4,18 @@ import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton, LongGrayButton } from '../formFields';
 
-
 import history from '../../history';
 
 class AccountInformationForm extends Component {
+
+    constructor() {
+        super()
+        
+        this.state = {
+            showPasswords: false
+        }
+    }
+
     render() {
         const { className, handleSubmit } = this.props;
     
@@ -15,7 +23,7 @@ class AccountInformationForm extends Component {
             <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
                 <Field className='account-information-form__name'
                 type='name'
-                title='name'
+                title='Name'
                 placeholder='Name'
                 name='name'
                 component={FormInput}/>
@@ -28,16 +36,17 @@ class AccountInformationForm extends Component {
 
                 <Field className='account-information-form__street-address'
                 type='address'
-                title='Street-Address'
-                placeholder='Street-Address'
+                title='Street Address'
+                placeholder='Street Address'
                 name='address'
                 component={FormInput}/>
                 <Field className='account-information-form__city'
                 type='city'
                 title='City'
-                placeholder='city'
+                placeholder='City'
                 name='city'
                 component={FormInput}/>
+
 
                 <Field className='account-information-form__state'
                 type='state'
@@ -51,19 +60,39 @@ class AccountInformationForm extends Component {
                 placeholder='Zipcode'
                 name='zipcode'
                 component={FormInput}/>
-
-
-
-
-               
-                <Field className='account-information-form__Change-password'
-                onClick={() => console.log('trying to show passwords')}
-                type='button'
-                labelTitle='Password'
-                title='Change Password'
-                name='change-Password'
-                component={LongGrayButton}/>
-                </form>
+                {
+                    this.state.showPasswords ?
+                        [
+                            <Field key={0} className='account-information-form__current'
+                            type='password'
+                            title='Current Password'
+                            placeholder='Current Password'
+                            name='current'
+                            component={FormInput}/>,
+                            <Field key={1} className='account-information-form__new'
+                            type='password'
+                            title='New Password'
+                            placeholder='New Password'
+                            name='new'
+                            component={FormInput}/>,
+                            <Field key={2} className='account-information-form__confirm'
+                            type='password'
+                            title='Confirm Password'
+                            placeholder='Confirm Password'
+                            name='confirm'
+                            component={FormInput}/>
+                        ]
+          
+                    :
+                        <Field className='account-information-form__change-password'
+                        onClick={() => this.setState({ showPasswords: true })}
+                        type='button'
+                        labelTitle='Password'
+                        title='Change Password'
+                        name='change-password'
+                        component={LongGrayButton}/>
+                }
+            </form>
         )
     }
 }
